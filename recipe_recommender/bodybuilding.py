5992ff7f11484a92e7f254a50d4632d1e69b3c47
Lisa <lisa.michelle.nash@gmail.com>
2018-12-27 13:07:02 -0500

    ingredient table and recipe ingredient tables

--- recipe_recommender/bodybuilding.py
+++ recipe_recommender/bodybuilding.py
@@ -9,6 +9,12 @@ import pandas as pd
 import requests
 import json
 
+DATA_DIR = '../data/'
+CLEANED_DATA_DIR = '../data/cleaned/'
+
+if not os.path.exists(CLEANED_DATA_DIR):
+    os.mkdir(CLEANED_DATA_DIR)
+
 
 def scrape_db():
     """
@@ -20,7 +26,7 @@ def scrape_db():
     # We want all of the recipes, so we'll do a quick request of one recipe to
     # get the 'total' number in the DB
     url_request = 'https://cms-api.bodybuilding.com/BbcomRecipe'
-    url_parameters = {'sort':'publishDate', 'order': 'desc', 'limit':'1'}
+    url_parameters = {'sort': 'publishDate', 'order': 'desc', 'limit': '1'}
 
     fake_recipes_list = requests.get(url_request, params=url_parameters)
     fake_recipes = bs4.BeautifulSoup(fake_recipes_list.content, features='html.parser')
@@ -30,7 +36,7 @@ def scrape_db():
     total_recipes = fake['total']
 
     # Change the 'limit' on the url to the total number of recipes
-    url_parameters = {'sort':'publishDate', 'order': 'desc', 'limit': str(total_recipes)}
+    url_parameters = {'sort': 'publishDate', 'order': 'desc', 'limit': str(total_recipes)}
 
     all_recipes_list = requests.get(url_request, params=url_parameters)
     all_recipes = bs4.BeautifulSoup(all_recipes_list.content, features='html.parser')
@@ -39,10 +45,13 @@ def scrape_db():
     all_recipes_list = json.loads(str(all_recipes))['_embedded']['bb-cms:search-results']
 
     # Dump to json file - results will always be saved in 'data' folder
-    save_path = os.path.join('..','data', 'bodybuilding_recipes.json')
+    save_path = os.path.join(DATA_DIR, 'bodybuilding_recipes.json')
     rf = open(save_path, 'w')
     json.dump(all_recipes_list, rf)
     rf.close()
+
+
+
 
 def save_df():
     """
@@ -94,3 +103,95 @@ def save_df():
 
     # Save as csv
     df.to_csv(save_path)
+
+
+
+
+def process_ingredients(data):
+    """
+       Process the ingredients from bodybuilding.com and save the recipe
+
+       data is the scraped json.
+
+    """
+
+    all_ingredients = []
+    all_amounts = []
+    all_units = []
+    all_descriptors = []
+    all_units = []
+    recipe_ids = []
+
+    for dat in data:
+        recipe_id = dat['id']
+        dat = dat['schemaOrg']
+        if 'recipeIngredient' in dat.keys():
+            ingredients = dat['recipeIngredient']
+            for ingredient in ingredients:
+                descriptor = None
+                recipe_ids.append(recipe_id)
+
+                ingredient = ingredient.replace(u"½", u".5")
+                ingredient = ingredient.replace(u"¼", u".25")
+                ingredient = ingredient.replace(u"¾", u".75")
+                ingredient = ingredient.replace(u"⅛", u".125")
+                split_ingredients = ingredient.split(' -')
+                this_ingredient = split_ingredients[0].strip()
+
+                split_again = split_ingredients[1].split(' ')
+
+                if len(split_again) > 2:
+                    amount = split_again[1]
+                    unit = split_again[2]
+                    try:
+                        amount = float(amount)
+                    except ValueError:
+                        amount = 0
+                        unit = None
+
+                else:
+                    amount = 0
+                    unit = None
+
+                with_descriptors = this_ingredient.split(',')
+                if len(with_descriptors) > 1:
+
+                    this_ingredient = with_descriptors[0]
+                    # print(with_descriptors)
+                    with_descriptors = with_descriptors[1:]
+
+                    descriptor = ' '.join(with_descriptors).strip().lower()
+
+
+                else:
+                    descriptor = None
+                # print(this_ingredient, float(amount), unit)
+
+                all_ingredients.append(this_ingredient.lower())
+                all_amounts.append(amount)
+                all_units.append(unit)
+                all_descriptors.append(descriptor)
+                # print('')
+    print(len(all_ingredients))
+
+    ingredient_set = sorted(list(set(all_ingredients)))
+    ingredient_table = pd.DataFrame({'ingredient':ingredient_set}).reset_index()
+
+    recipe_ingredient_table = pd.DataFrame(
+        {'recipe_id': recipe_ids, 'ingredient': all_ingredients, 'amount': all_amounts, 'unit':all_units,
+         'descriptor': all_descriptors})
+
+    ingredient_table['index'] += 1
+    ingredient_table = ingredient_table.rename(columns={'index': 'ingredient_id'})
+    recipe_ingredient_table = recipe_ingredient_table.merge(ingredient_table, left_on='ingredient', right_on='ingredient')
+
+    recipe_ingredient_table.to_csv(CLEANED_DATA_DIR + 'recipe_ingredients.csv', index=False)
+    ingredient_table.to_csv(CLEANED_DATA_DIR + 'ingredients.csv', index=False)
+
+
+if __name__ == '__main__':
+
+    with open(DATA_DIR + 'bodybuilding_recipes.json') as f:
+        scraped_data = json.load(f)
+
+    process_ingredients(scraped_data)
